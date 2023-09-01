4dc8d51125b97a668ef678f8f783d7a9463c9da5
Robert Adamski <114213959+RobertAdams09@users.noreply.github.com>
2023-09-01 16:21:21 +0200

    Add files via upload

---
+++ Birthday Trial/src/data/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        name: 'Brian Dylan',
+        age: 23,
+        image: 'https://images.unsplash.com/photo-1591084728795-1149f32d9866?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80',
+    },
+    {
+        id: 2,
+        name: 'Dylan Brian',
+        age: 32,
+        image: 'https://images.unsplash.com/photo-1581382575275-97901c2635b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        id: 3,
+        name: 'Brian Nalyd',
+        age: 22,
+        image: 'https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        id: 4,
+        name: 'Dylan Nairb',
+        age: 33,
+        image: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+        id: 5,
+        name: 'Bin Radlan',
+        age: 323,
+        image: 'https://images.unsplash.com/photo-1618886614638-80e3c103d31a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=370&q=80',
+    },
+];
+
+export default data;
